eb67f9e1885415f583142abe377532fbda35bad3
Ishant_Punyani <punyani.ishant@gmail.com>
2024-12-21 17:19:45 +0530

    PartnersAndSponsors

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css'
 import Celebrating10Years from "./components/Celebrating10Years";
 import DroomBrandRecognition from "./components/DroomBrandRecognition";
 import DroomAchievements from "./components/DroomAchievements";
+import PartnersAndSponsors from "./components/PartnersAndSponsors";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Celebrating10Years />
       <DroomBrandRecognition />
       <DroomAchievements />
+      <PartnersAndSponsors />
     </div>
   );
 }
---
+++ src/components/PartnersAndSponsors.js
@@ -0,0 +1,52 @@
+import React from "react";
+import "../styles/PartnersAndSponsors.css";
+import "../styles/Pre-Defined-Fonts.css";
+
+function PartnersAndSponsors() {
+  return (
+    <div className="partners-and-sponsors">
+      <h1 className="heading-pas Inter-Bold-48px">Partners and Sponsors</h1>
+      <div className="sponsors-images">
+        <div id="sponsor-img-1" className="sponsor-img"></div>
+        <div id="sponsor-img-2" className="sponsor-img"></div>
+        <div id="sponsor-img-3" className="sponsor-img"></div>
+        <div id="sponsor-img-4" className="sponsor-img"></div>
+        <div id="sponsor-img-5" className="sponsor-img"></div>
+        <div id="sponsor-img-6" className="sponsor-img"></div>
+        <div id="sponsor-img-7" className="sponsor-img"></div>
+        <div id="sponsor-img-8" className="sponsor-img"></div>
+        <div id="sponsor-img-9" className="sponsor-img"></div>
+        <div id="sponsor-img-10" className="sponsor-img"></div>
+        <div id="sponsor-img-11" className="sponsor-img"></div>
+        <div id="sponsor-img-12" className="sponsor-img"></div>
+      </div>
+      <div className="ten-img">
+        <h1 className="Inter-Bold-48px heading-pas-2">
+          Our Unmatched Scale & Impact
+        </h1>
+      </div>
+      <h3 id="ten-h-1" className="Inter-Semibold-32px">16Mn+</h3>
+      <p id="ten-p-1" className="Inter-Regular-20px">Traffic (per Month)</p>
+      <h3 id="ten-h-2" className="Inter-Semibold-32px">16.2Mn+</h3>
+      <p id="ten-p-2" className="Inter-Regular-20px">App downloads</p>
+      <h3 id="ten-h-3" className="Inter-Semibold-32px">1174+</h3>
+      <p id="ten-p-3" className="Inter-Regular-20px">No. of cities</p>
+      <h3 id="ten-h-4" className="Inter-Semibold-32px">$7.4Bn+</h3>
+      <p id="ten-p-4" className="Inter-Regular-20px">GMV</p>
+      <h3 id="ten-h-5" className="Inter-Semibold-32px">582k+</h3>
+      <p id="ten-p-5" className="Inter-Regular-20px">Vehicle Sold</p>
+      <h3 id="ten-h-6" className="Inter-Semibold-32px">7Mn+</h3>
+      <p id="ten-p-6" className="Inter-Regular-20px">Social Media Followers</p>
+      <h3 id="ten-h-7" className="Inter-Semibold-32px">354k+</h3>
+      <p id="ten-p-7" className="Inter-Regular-20px">Dealers</p>
+      <h3 id="ten-h-8" className="Inter-Semibold-32px">$7.4Bn+</h3>
+      <p id="ten-p-8" className="Inter-Regular-20px">Listed GMV</p>
+      <h3 id="ten-h-9" className="Inter-Semibold-32px">250k+</h3>
+      <p id="ten-p-9" className="Inter-Regular-20px">Listing</p>
+      <h3 id="ten-h-10" className="Inter-Semibold-32px">200Mn+</h3>
+      <p id="ten-p-10" className="Inter-Regular-20px">Traffic (per Year)</p>
+    </div>
+  );
+}
+
+export default PartnersAndSponsors;
